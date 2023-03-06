ef05cd7a07e2d2553eabe4deb7f2a815bae64673
HarsanyiBalazs <102156614+HarsanyiBalazs@users.noreply.github.com>
2023-03-06 12:43:24 +0100

    Add files via upload

---
+++ feladat1_9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    int number = atoi(argv[2]);
+    int minval = atoi(argv[3]);
+    int maxval = atoi(argv[4]);
+    FILE *fptr;
+    int numbers[number];
+
+    srand(time(0));
+
+    for (int i = 0; i < number; i++)
+    {
+        
+        numbers[i] = rand()%(maxval-minval+1) + minval ;
+
+    }
+    
+
+    fptr = fopen(argv[1],"w");
+
+    if(fptr == NULL)
+    {
+      printf("Error!");   
+      return 1;             
+    }
+
+   for (int i = 0; i < number; i++)
+   {
+    
+        fprintf(fptr," %d ",numbers[i]);
+
+   }
+   return 0;
+}
