c4917edb42a413d7c79997f85b9ece1939489fc7
HarsanyiBalazs <102156614+HarsanyiBalazs@users.noreply.github.com>
2023-03-11 20:54:45 +0100

    Update feladat1_9.c

--- feladat1_9.c
+++ feladat1_9.c
@@ -5,35 +5,72 @@
 int main(int argc, char *argv[])
 {
     int number = atoi(argv[2]);
+    int numberhelp=number;
     int minval = atoi(argv[3]);
     int maxval = atoi(argv[4]);
     FILE *fptr;
-    int numbers[number];
+    FILE *fptrtime;
+    int numbers[number*20];
+
+
+    clock_t startrand,endrand,startwrite,endwrite;
+    double timespent1,timespent2;
 
     srand(time(0));
-
-    for (int i = 0; i < number; i++)
-    {
-        
-        numbers[i] = rand()%(maxval-minval+1) + minval ;
-
-    }
-    
-
     fptr = fopen(argv[1],"w");
 
     if(fptr == NULL)
     {
-      printf("Error!");   
-      return 1;             
+      printf("Error!");
+      return 1;
     }
 
-   for (int i = 0; i < number; i++)
-   {
-    
-        fprintf(fptr," %d ",numbers[i]);
+    fptrtime = fopen("data.txt","a");
 
-   }
-   fclose(fptr); 
+    if(fptrtime == NULL)
+    {
+      printf("Error!");
+      return 1;
+    }
+
+    for(int j=number;j<=(numberhelp*20);j=j+numberhelp)
+    {
+
+        startrand=clock();
+
+        for (int i = 0; i < number; i++)
+        {
+
+            numbers[i] = rand()%(maxval-minval+1) + minval ;
+
+        }
+
+
+        endrand=clock();
+        timespent1 = ((double)(endrand-startrand))/ CLOCKS_PER_SEC;
+
+        startwrite=clock();
+
+        for (int i = 0; i < number; i++)
+        {
+
+            fprintf(fptr," %d ",numbers[i]);
+
+        }
+
+
+        endwrite=clock();
+        timespent2 = ((double)(endwrite-startwrite))/CLOCKS_PER_SEC;
+
+        fprintf(fptrtime,"%d %lf %lf\n",number,timespent1,timespent2);
+
+        number=number+numberhelp;
+
+
+
+    }
+
+   fclose(fptrtime);
+   fclose(fptr);
    return 0;
 }
