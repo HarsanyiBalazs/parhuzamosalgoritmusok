ef46b424bb9b6b8b2b3e0dc9544137e82fd126ff
HarsanyiBalazs <102156614+HarsanyiBalazs@users.noreply.github.com>
2023-03-11 18:16:25 +0100

    Update feladat1_7.c

--- feladat1_7.c
+++ feladat1_7.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 int main()
 {
-    int n = 30;
+    int n = 20000;
     int db = 0;
     int prime=1;
 
+    clock_t start,end;
+    double timespent;
+
+    FILE *fptr;
+    fptr = fopen("timespentprimes.txt","a");
+
+    if(fptr == NULL)
+    {
+      printf("Error!");
+      return 1;
+    }
+
+
+
+    for(int k=1000 ;k<=n;k=k+1000){
+    start=clock();
     for (int i = 2; i <= n; i++)
     {
-        
+
 
         for ( int j = 2; j <= (i/2) ; j++)
         {
-            
+
             if ( (i % j) == 0 )
             {
                 prime=0;
                 break;
             }
-           
+
         }
 
         if (prime == 1)
@@ -28,7 +46,14 @@ int main()
         }
         prime = 1 ;
     }
-    
+    end = clock();
+    printf("%d\n",db);
+    timespent = ((double)(end-start))/ CLOCKS_PER_SEC;
+    fprintf(fptr,"%lf %d\n",timespent,k);
+    }
+
+
+    fclose(fptr);
     printf("%d",db);
     return 0;
 }
